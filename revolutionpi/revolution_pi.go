8a7f578a3b2fe681a2686859355d70fd16d1a356
John <john@viam.com>
2024-05-01 11:55:43 -0400

    update driver version

--- revolutionpi/revolution_pi.go
+++ revolutionpi/revolution_pi.go
@@ -47,7 +47,7 @@ func newBoard(
 	conf resource.Config,
 	logger logging.Logger,
 ) (board.Board, error) {
-	logger.Info("Starting RevolutionPi Driver v0.0.6")
+	logger.Info("Starting RevolutionPi Driver v0.0.7")
 
 	devPath := filepath.Join("/dev", "piControl0")
 	devPath = filepath.Clean(devPath)
